4d86ffee2fe2c549426dd53dcc4519051c2cfeca
Gregory McWhirter <greg@ideafreemonoid.org>
2012-10-10 20:50:21 -0700

    Adding library to redirect node console output to the 'browser'

--- Jakefile.js
+++ Jakefile.js
@@ -8,11 +8,12 @@ var path = require('path')
 desc('build all assets');
 task('build', [ 'build:component'
               , 'build:' + path.join('data', 'content', 'js', 'build.js')
-              , 'build:' + path.join('data', 'content', 'css', 'build.css')    
+              , 'build:' + path.join('data', 'content', 'css', 'build.css')
               , 'build:' + path.join('data', 'content', 'index.html')
               , 'build:' + path.join('data', 'content', 'css', 'style.css')
               ], function (){
-  
+                console.log();
+                console.log("Done building all tasks.");
 });
 
 function copy(src, dest){
@@ -23,23 +24,26 @@ function copy(src, dest){
 namespace('build', function (){
 
   desc('creates data/content directory');
-  directory(path.join('data', 'content'));
-  
+  directory(path.join('data', 'content'), function (){
+    console.log("Creating directory %s", path.join('data', 'content'));
+  });
+
   desc('creates data/content/css directory');
   directory(path.join('data', 'content', 'css'), [path.join('data', 'content')]);
-  
+
   desc('creates data/content/js directory');
   directory(path.join('data', 'content', 'js'), [path.join('data', 'content')]);
-  
+
   desc('creates src/js/build directory');
   directory(path.join('src', 'js', 'build'));
-  
+
   desc('creates src/js/components directory');
   directory(path.join('src', 'js', 'components'));
-  
+
   desc('build component pieces');
   task('component', [path.join('data', 'content', 'js'), path.join('src', 'js', 'build'), path.join('src', 'js', 'components')], function (){
-  
+    console.log();
+    console.log("Downloading and building components...");
     //The following is essentially copied from component/component/bin/component-install and component/component/bin/component-build
     var component = require('component')
       , outdir = path.join('src', 'js')
@@ -47,27 +51,28 @@ namespace('build', function (){
       , pkgs = []
       , Builder = require('component-builder')
       ;
-      
+
       if (config.dependencies){
         pkgs = pkgs.concat(normalize(config.dependencies));
       }
-      
+
       var pkgcount = pkgs.length;
       var pkgsdone = 0;
-      
+
       function whenDone(err){
         if (err) {
-          throw err; 
+          throw err;
         }
         else {
           pkgsdone += 1;
+          //console.log("done %s/%s", pkgsdone, pkgcount);
         }
-        
+
         if (pkgsdone === pkgcount){
           doBuild();
         }
       }
-      
+
       pkgs.forEach(function (pkg){
         var parts = pkg.split('@');
         pkg = parts.shift();
@@ -78,16 +83,15 @@ namespace('build', function (){
         , dev: false
         });
         report(pkg);
-        pkg.on('end', whenDone);
         pkg.install();
       });
-      
+
       function normalize(deps) {
         return Object.keys(deps).map(function(name){
           return name + '@' + deps[name];
         });
       }
-      
+
       function report(pkg) {
         console.log('install', pkg.name + '@' + pkg.version);
 
@@ -103,6 +107,7 @@ namespace('build', function (){
 
         pkg.on('exists', function(dep){
           console.log('exists', dep.name + '@' + dep.version);
+          whenDone();
         });
 
         pkg.on('file', function(file){
@@ -111,9 +116,10 @@ namespace('build', function (){
 
         pkg.on('end', function(){
           console.log('complete', pkg.name);
+          whenDone();
         });
       }
-      
+
       function doBuild(){
         //do component build
         var js = fs.createWriteStream(path.join(outdir, 'build', 'build.js'));
@@ -122,11 +128,11 @@ namespace('build', function (){
         // build
 
         var builder = new Builder(outdir);
-        console.log(builder);
-        console.log(builder.path("component.json"));
+        //console.log(builder);
+        //console.log(builder.path("component.json"));
         var start = new Date;
 
-        console.log();
+        //console.log();
         builder.build(function(err, obj){
           if (err) {
             throw err;
@@ -148,22 +154,25 @@ namespace('build', function (){
           complete();
         });
       }
-      
+
   }, {async: true});
-  
+
   desc('copies built component js files to dist');
-  file(path.join('data', 'content', 'js', 'build.js'), ['component', path.join('data', 'content', 'js'), path.join('src', 'js', 'build', 'build.js')], function (){
+  file(path.join('data', 'content', 'js', 'build.js'), ['component', path.join('data', 'content'), path.join('data', 'content', 'js'), path.join('src', 'js', 'build', 'build.js')], function (){
+    console.log();
     copy(path.join('src', 'js', 'build', 'build.js'), path.join('data', 'content', 'js', 'build.js'));
   });
-  
+
   desc('copies build component css files to dist');
-  file(path.join('data', 'content', 'css', 'build.css'), [path.join('data', 'content', 'css'), path.join('src', 'js', 'build', 'build.css')], function (){
+  file(path.join('data', 'content', 'css', 'build.css'), [path.join('data', 'content'), path.join('data', 'content', 'css'), path.join('src', 'js', 'build', 'build.css')], function (){
+    console.log();
     copy(path.join('src', 'js', 'build', 'build.css'), path.join('data', 'content', 'css', 'build.css'));
   });
-  
+
   desc('builds stylus files')
-  file(path.join('data', 'content', 'css', 'style.css'), [path.join('data', 'content', 'css'), path.join('src', 'stylus', 'style.styl')], function (){
-    console.log("Generating layout.css from stylus.");
+  file(path.join('data', 'content', 'css', 'style.css'), [path.join('data', 'content'), path.join('data', 'content', 'css'), path.join('src', 'stylus', 'style.styl')], function (){
+    console.log();
+    console.log("Generating style.css from stylus.");
 
     var src = fs.readFileSync(path.join('src', 'stylus', 'style.styl'), "utf8");
 
@@ -173,9 +182,10 @@ namespace('build', function (){
         complete();
     });
   }, {async: true});
-  
+
   desc('builds jade index file');
   file(path.join('data', 'content', 'index.html'), [path.join('data', 'content'), path.join('src', 'jade', 'index.jade')], function (){
+    console.log();
     console.log("Generating index.html from jade.");
     var src = fs.readFileSync(path.join('src', 'jade', 'index.jade'))
       , fn = jade.compile(src, {filename: path.join('src', 'jade', 'index.jade')})
@@ -184,6 +194,6 @@ namespace('build', function (){
 
     fs.writeFileSync(path.join('data', 'content', 'index.html'), fn(locals));
   });
-  
-  
+
+
 });
--- data/app.js
+++ data/app.js
@@ -1,12 +1,16 @@
-var app = module.exports = require('appjs');
+var app = module.exports = require('appjs')
+  , ConsoleStream = require('console_stream')
+  ;
 
-app.serveFilesFrom(__dirname + '/dist');
+app.serveFilesFrom(__dirname + '/content');
 
 var window = app.createWindow({
-  width  : 640,
-  height : 460,
+  width  : 800,
+  height : 500,
   icons  : __dirname + '/icons'
 });
+
+console.log(window);
 
 window.on('create', function(){
   console.log("Window Created");
@@ -19,6 +23,16 @@ window.on('ready', function(){
   window.node_process = process;
   window.node_module = module;
 
+  var consoleStream = new ConsoleStream(window.console)
+    , desc = { configurable: true,
+             enumerable: true,
+             get: function(){ return consoleStream } };
+
+  Object.defineProperties(process, {
+    stderr: desc,
+    stdout: desc
+  });
+
   function F12(e){ return e.keyIdentifier === 'F12' }
   function Command_Option_J(e){ return e.keyCode === 74 && e.metaKey && e.altKey }
 
@@ -27,12 +41,14 @@ window.on('ready', function(){
       window.frame.openDevTools();
     }
   });
-  
+
   window.addEventListener('app-done', function(e){
     console.log(e);
   });
-  
+
   window.dispatchEvent(new window.Event('app-ready'));
+
+  console.log("Application bindings set up.");
 });
 
 window.on('close', function(){
